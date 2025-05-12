2523da80a2f0f8f5c6537b763c103844ec14c245
Delaneylekeanyi <zingwadelaney1@gmail.com>
2025-05-12 06:12:43 +0100

    rendering html pages with expres

--- my_app/index.js
+++ my_app/index.js
@@ -1,11 +1,15 @@
 // index.js
 const express = require('express');
 const app = express();
+const path = require('path')
 const port = 3000;
 
+
+//rendering html pages with express
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
